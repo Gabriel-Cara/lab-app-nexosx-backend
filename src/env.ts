0403efdd3401db92ebc40098f3106971f9ad9c80
Gabriel-Cara <gabricar28@gmail.com>
2025-10-20 10:58:35 -0300

    add: starting project

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  JWT_SECRET: z.string(),
+  DATABASE_URL: z.string(),
+  NOTIFICATION_PROVIDER: z.string().default("console"),
+  PORT: z.coerce.number().default(3333)
+});
+
+export const env = envSchema.parse(process.env);
